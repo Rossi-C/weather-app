8c9a2b72980acd8f08cd0071fb2e7151b7329d84
Rossi-C <113311778+Rossi-C@users.noreply.github.com>
2022-10-20 17:00:51 -0400

    added user input field for city and country. coded dynamic display for weather data
    form to take in city and country input.
    button and handleclick function to update weather data with input.

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,17 +1,32 @@
 import './App.css';
 import { useState, useEffect } from 'react';
-import { getWeatherData } from './api'
+import { getWeatherData } from './api';
 
 function App() {
   const [temp, setTemp] = useState(null);
   const [description, setDescription] = useState(null);
   const [humidity, setHumidity] = useState(null);
   const [windSpeed, setWindSpeed] = useState(null);
+  const [city, setCity] = useState('Charleston');
+  const [country, setCountry] = useState('US');
+  const [isImperial, setIsImperial] = useState(true);
 
-  const updateWeather = async () => {
+  const updateWeather = async (cityName, countryCode) => {
+    let countryLower = countryCode.toLowerCase();
     try {
-      let WeatherData = await getWeatherData('mcconnells', 'us');
-      console.log(WeatherData);
+      if (countryLower === 'us' || countryLower === 'mm' || countryLower === 'lr') {
+        setIsImperial(true);
+      } else {
+        setIsImperial(false);
+      }
+      let { main: { temp, humidity }, weather: [{ description }], wind: { speed } } = await getWeatherData(cityName, countryLower);
+      console.log(speed);
+      if (temp || humidity || description || speed) {
+        setTemp(temp);
+        setHumidity(humidity);
+        setDescription(description);
+        setWindSpeed(speed);
+      }
     } catch (err) {
       console.log(`data not found`);
       console.log(err);
@@ -19,12 +34,61 @@ function App() {
   }
 
   useEffect(() => {
-    updateWeather();
+    updateWeather(city, country);
+    console.log(windSpeed);
   }, [])
+
+  const handleClick = async (e) => {
+    e.preventDefault();
+    console.log(city, country);
+    await updateWeather(city, country);
+  }
 
   return (
     <div className="App">
-      <h1>Poopy</h1>
+      <h1>Global Weather</h1>
+      <p>Enter your city and country to get started!</p>
+      <form>
+        <label>
+          City:
+          <input
+            className='Input'
+            type='text'
+            name='city'
+            value={city}
+            onChange={e => setCity(e.target.value)} />
+        </label>
+        <br></br>
+        <label>
+          Country:
+          <input
+            className='Input'
+            type='text'
+            name='country'
+            value={country}
+            onChange={e => setCountry(e.target.value)} />
+        </label>
+      </form>
+      <button
+        className='Input'
+        type='button'
+        onClick={handleClick}>
+        Search Weather
+      </button>
+      <div className='Description'>
+        {isImperial ?
+          <p>Temperature: {temp}&deg;F</p>
+          :
+          <p>Temperature: {temp}&deg;C</p>
+        }
+        <p>Weather Condition: {description} </p>
+        <p>Humidity: {humidity}% </p>
+        {isImperial ?
+          <p>Wind Speed: {windSpeed} Miles per hour </p>
+          :
+          <p>Wind Speed: {windSpeed} Meters per Second</p>
+        }
+      </div>
     </div>
   );
 }
--- weather-app/src/api/index.js
+++ weather-app/src/api/index.js
@@ -2,7 +2,14 @@
 
 export const getWeatherData = async (cityName, countryCode) => {
     const apiKey = '6c7251e821c348e7a6680bfec62adfa8';
-    let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName},${countryCode}&appid=${apiKey}&units=imperial`;
+    const countryLower = countryCode.toLowerCase();
+    let units;
+    if (countryLower === 'us' || countryLower === 'mm' || countryLower === 'lr') {
+        units = 'imperial';
+    } else {
+        units = 'metric';
+    }
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName},${countryCode}&appid=${apiKey}&units=${units}`;
     const response = await fetch(url);
     const weather = await response.json();
     return weather
